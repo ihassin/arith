752674209ccd8fc525aec99c8188b314fee0b583
Itamar Hassin <ihassin@mac.com>
2018-07-23 11:36:06 -0400

    Split code to to files

--- lib/substraction.c
+++ lib/substraction.c
@@ -4,60 +4,6 @@
 
 #include "arith.h"
 
-#define ABS(x)  ((x) <= 0 ? -(x) : (x))
-
-char *add(char *first, char *second)
-{
-    size_t len1 = strlen(first);
-    size_t len2 = strlen(second);
-    size_t maxLen = MAX(len1, len2);
-
-    char *addition = malloc(maxLen + 1 + ABS(len1 - len2));
-    char *additionPtr = addition;
-
-    char *numberPtr = first + len1 - 1;
-    char *fromPtr = second + len2 - 1;
-
-    memset(addition, 0, maxLen);
-
-    char borrow = 0;
-    while((numberPtr >= first) && (fromPtr >= second))
-    {
-        char num1 = *numberPtr + borrow - '0';
-        char num2 = *fromPtr - '0';
-        char sub = num1 + num2;
-
-        if(sub >= 10)
-        {
-            *additionPtr++ = (sub - 10) + '0';
-            borrow = 1;
-        } else
-        {
-            *additionPtr++ = sub + '0';
-            borrow = 0;
-        }
-        numberPtr--;
-        fromPtr--;
-    }
-
-    if(borrow)
-    {
-        *additionPtr++ = '1';
-    }
-
-    while(numberPtr >= first) {
-        *additionPtr++ = *numberPtr--;
-    }
-
-    while(fromPtr >= second) {
-        *additionPtr++ = *fromPtr--;
-    }
-
-    *additionPtr = 0;
-    additionPtr = strrev(addition);
-    return(additionPtr);
-}
-
 char *substract(char *number, char *from)
 {
     size_t len1 = strlen(number);
